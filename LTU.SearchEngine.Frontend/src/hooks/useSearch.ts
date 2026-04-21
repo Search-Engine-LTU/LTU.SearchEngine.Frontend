7989aac620cbdb121caaadc08990ac3ac8a087da
Jean-Paul Hanna <101112377+Arnith86@users.noreply.github.com>
2026-04-21 15:58:30 +0200

    Basic connection to backend API set up.

--- LTU.SearchEngine.Frontend/src/hooks/useSearch.ts
+++ LTU.SearchEngine.Frontend/src/hooks/useSearch.ts
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { validateQuery } from '../validation/SearchValidator';
 import { mockSearchResults } from './mockData';
 import type { SearchResponse } from '../models/SearchInterface';
+import { fetchFromApi } from '../api/SearchService';
 
 export const useSearch = () => {
     const [error, setError] = useState<string | undefined>();
@@ -16,52 +17,54 @@ export const useSearch = () => {
         setWarning(validation.warningMessage);
         setSearchData(undefined);
 
-        if (!validation.isValid) {
-            return; 
-        }
+        if (!validation.isValid) return; 
+        
 
-           try {
-        setIsLoading(true);
+        try {
 
-        // simulating API delay (NFRQ-6001 req: response within 10s)
-        await new Promise(resolve => setTimeout(resolve, 800));
+            setIsLoading(true);
+            const data = await fetchFromApi(query, page)
+            // // simulating API delay (NFRQ-6001 req: response within 10s)
+            // await new Promise(resolve => setTimeout(resolve, 800));
 
-            //if user searches on word "empty" we pretend that backend doesnt find anything
-            if(query.toLowerCase() === "empty") {
-                setSearchData({
-                  results: [],
-                totalResults: 0,
-                currentPage: 1,
-                pageSize: 10,
-                totalPages: 0  
-                });
-                return;
-            }
+            //     //if user searches on word "empty" we pretend that backend doesnt find anything
+            //     if(query.toLowerCase() === "empty") {
+            //         setSearchData({
+            //             results: [],
+            //             totalResults: 0,
+            //             currentPage: 1,
+            //             pageSize: 10,
+            //             totalPages: 0  
+            //         });
 
-        setSearchData(mockSearchResults);
-            // Here we going to make our API call to backend.api (searchcontroller)
-            // Exempel: const response = await FrontendClient.get(`/api/search?q=${encodeURIComponent(query)}`);
-            // setResults(response.data);
+            //         return;
+            //     }
+
+            setSearchData(mockSearchResults);
+                // Here we going to make our API call to backend.api (searchcontroller)
+                // Exempel: const response = await FrontendClient.get(`/api/search?q=${encodeURIComponent(query)}`);
+                // setResults(response.data);
 
             const pageSize = 10;
-        const startIndex = (page - 1) * pageSize;
-        const endIndex = startIndex + pageSize;
+            const startIndex = (page - 1) * pageSize;
+            const endIndex = startIndex + pageSize;
 
-        const paginatedResults = mockSearchResults.results.slice(startIndex, endIndex);
-            
-        setSearchData({
-            ...mockSearchResults,
-            results: paginatedResults,
-            currentPage: page
-        });
+            const paginatedResults = mockSearchResults.results.slice(startIndex, endIndex);
+                
+            setSearchData({
+                ...mockSearchResults,
+                results: paginatedResults,
+                currentPage: page
+            });
 
             console.log("Valid query sent to API:", query); 
+
         } catch (err) {
-            setError("A network error occurred while searching.");
+            setError(`A network error occurred while searching. Err: ${err}`);
         } finally {
             setIsLoading(false);
         }
     };
 
-return { executeSearch, searchData, error, warning, isLoading };
+    return { executeSearch, searchData, error, warning, isLoading };
 };
